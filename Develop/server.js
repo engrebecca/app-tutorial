a5c8fa538508684463864c1b22e0f8a520602695
reng <reng412@gmail.com>
2020-10-27 20:16:10 -0700

    Add comments to server.js file

--- Develop/server.js
+++ Develop/server.js
@@ -10,21 +10,26 @@ var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
 var app = express();
+// Middleware that parses incoming requests that are urlencoded
 app.use(express.urlencoded({ extended: true }));
+// Middleware that parses incoming requests that are JSON
 app.use(express.json());
+// Middleware that serves static files that are in the public directory, combines req.url with the public directory to form a new file path
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+// Middleware to initialize passport for authentication
 app.use(passport.initialize());
+// Middlware that updates the request object's user value to the true user object that is deserialized
 app.use(passport.session());
 
-// Requiring our routes
+// Requiring our HTML and API routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
 });
