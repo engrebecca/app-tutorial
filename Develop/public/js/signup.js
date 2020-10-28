78bd34502a4b693a0459887250180b7fb23c317c
reng <reng412@gmail.com>
2020-10-27 18:56:50 -0700

    Add comments to signup.js file

--- Develop/public/js/signup.js
+++ Develop/public/js/signup.js
@@ -1,17 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // Getting references to our form and input
   var signUpForm = $("form.signup");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
 
   // When the signup button is clicked, we validate the email and password are not blank
-  signUpForm.on("submit", function(event) {
+  signUpForm.on("submit", function (event) {
     event.preventDefault();
+    // Creates an object that contains the email and password values from the form that are trimmed
     var userData = {
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
     };
 
+    // If the email or password are blank, return
     if (!userData.email || !userData.password) {
       return;
     }
@@ -28,7 +30,7 @@ $(document).ready(function() {
       email: email,
       password: password
     })
-      .then(function(data) {
+      .then(function (data) {
         window.location.replace("/members");
         // If there's an error, handle it by throwing up a bootstrap alert
       })
